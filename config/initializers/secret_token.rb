2c1ff2ceedb003bfb92fb568c9a6d61cb92f82cc
Shinkyo Will Warner <gwmf7rru35@snkmail.com>
2013-11-03 18:10:34 -0500

    Creates empty Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tripleworld::Application.config.secret_token = '6a8c6dc9e51c6d7e82976f3336dee693692400312126be892490fc7fb3d2e420573df07061633c1b6c9b2038ab4c3d95e038626b995c484c4684d5d74ee3d764'
