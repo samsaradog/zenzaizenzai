d351c2be6e576b1814d1831dcaf5471e64901367
Shinkyo Will Warner <gwmf7rru35@snkmail.com>
2013-12-23 12:10:23 -0500

    Adds default url options to production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,7 @@ Zenzai::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {:host => "zenzaizenzai.com"}
 
   # Enable threaded mode
   # config.threadsafe!
